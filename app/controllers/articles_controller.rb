33bda58a696ebd941683043c770aa57220fd1051
obevanken <vanen11@bk.ru>
2018-04-25 21:29:29 +0700

    blog

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,25 @@
+class ArticlesController < ApplicationController
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def index
+    @articles = Article.all
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(params.require(:article).permit(:title, :text))
+
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
+  end
+
+end
